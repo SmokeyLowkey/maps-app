932f38f290e3afff7908cfecfbf2e6b53c6967d6
SmokeyLowkey <yuriykondakov8@gmail.com>
2024-04-19 00:00:51 -0600

    modified:   BACKEND/db/db/settings.py

--- BACKEND/db/db/settings.py
+++ BACKEND/db/db/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.awsapprunner.com']
+ALLOWED_HOSTS = ['https://maps-app-6w2k.onrender.com']
 
 
 # Application definition
