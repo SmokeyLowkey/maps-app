ccaae26fb45d0950d4e048595e19598526ae10bc
SmokeyLowkey <yuriykondakov8@gmail.com>
2024-04-30 00:27:58 -0600

    modified:   BACKEND/db/data/__pycache__/views.cpython-312.pyc 	modified:   BACKEND/db/data/views.py 	modified:   BACKEND/db/db/__pycache__/settings.cpython-312.pyc 	modified:   BACKEND/db/db/__pycache__/urls.cpython-312.pyc 	modified:   BACKEND/db/db/urls.py

--- BACKEND/db/data/views.py
+++ BACKEND/db/data/views.py
@@ -3,6 +3,9 @@
 from rest_framework import generics
 from .models import BranchData
 from .serializers import BranchDataSerializer
+import os
+import requests
+from django.http import JsonResponse
 
 class BranchDataList(generics.ListCreateAPIView):
     queryset = BranchData.objects.all()
@@ -12,3 +15,19 @@ class BranchDataDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = BranchData.objects.all()
     serializer_class = BranchDataSerializer
     lookup_field = 'code'  # Using 'code' as the lookup field instead of the default 'id'
+
+# Django view for handling Mapbox API requests
+def mapbox_proxy(request):
+    # Extract query parameters from frontend request
+    endpoint = request.GET.get('endpoint')  # e.g., 'geocoding/v5/mapbox.places'
+    query = request.GET.get('query')  # e.g., the address to geocode
+
+    mapbox_url = f"https://api.mapbox.com/{endpoint}/{query}.json"
+    mapbox_params = {
+        'access_token': os.getenv('MAPBOX_API_KEY'),
+        'limit': '1'
+    }
+
+    response = requests.get(mapbox_url, params=mapbox_params)
+    return JsonResponse(response.json())
+
--- BACKEND/db/db/urls.py
+++ BACKEND/db/db/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from data.views import BranchDataList, BranchDataDetail
+from data.views import BranchDataList, BranchDataDetail, mapbox_proxy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('branchdata/', BranchDataList.as_view(), name='branchdata-list-create'),
     path('branchdata/<str:code>/', BranchDataDetail.as_view(), name='branchdata-detail'),
+    path('api/mapbox-proxy/', mapbox_proxy, name='mapbox_proxy'),
 ]
